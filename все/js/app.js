0ba3f42c8062556537a3a04148a0326fec8d000e
Groash <103875019+Groash@users.noreply.github.com>
2022-04-23 09:26:20 +0500

    Add files via upload

---
+++ все/js/app.js
@@ -0,0 +1,38 @@
+// Работает с ссылками
+
+const links = document.querySelectorAll(".link");
+
+links.forEach(link=>{
+    link.addEventListener("click",()=>{
+        links.forEach(ele => ele.classList.remove("active"));
+        link.classList.add("active");
+    })
+})
+
+
+//contact form
+
+const contactBtn = document.querySelector('.contact-btn');
+const firstName = document.querySelector('.first-name');
+const lastName = document.querySelector('.last-name');
+const email = document.querySelector('.email');
+const msg = document.querySelector('.message');
+
+contactBtn.addEventListener('click', () => {
+    if(firstName.value.length && lastName.value.length && email.value.length && msg.value.length){
+        fetch('/mail', {
+            method: 'post',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({
+                firstname: firstName.value,
+                lastname: lastName.value,
+                email: email.value,
+                msg: msg.value,
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            alert(data);
+        })
+    }
+})
